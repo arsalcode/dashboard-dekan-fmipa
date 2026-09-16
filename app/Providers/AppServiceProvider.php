797bc0c37a4a1c021cc54c0218073ae683fcaa98
arsalcode <email-arsalcode404@gmail.com>
2026-09-16 10:11:09 +0700

    Fix mixed-content SSL by forcing HTTPS for reverse proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (
+            (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')
+            || str_contains(request()->header('host', ''), 'trycloudflare.com')
+            || str_contains(request()->header('host', ''), 'ngrok')
+            || str_contains(request()->header('host', ''), 'loca.lt')
+        ) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
